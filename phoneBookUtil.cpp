ed383b326b0e48a6f0e215122ce162104fad8987
Martell <vovasamb@yandex.ru>
2022-09-19 16:43:47 +0300

    Header of task 2

---
+++ phoneBookUtil.cpp
@@ -0,0 +1,29 @@
+#include "phoneBookUtil.h"
+
+void convertTextToBinary(const string& textFilename, const string& binFilename){
+
+}
+
+void convertBinaryToText(const string& binFilename, const string& textFilename){
+
+}
+
+void printBinaryFile(const string& filename){
+
+}
+
+PhoneUser getRecord(const string& filename, int n){
+
+}
+
+void deleteRecord(const string& filename, int n){
+
+}
+
+void putAllStartsWith(const string& binFilename, const string& textFilename, const string& start){
+
+}
+
+void deleteAllStartsWith(const string& filename, const string& start){
+
+}
--- phoneBookUtil.h
+++ phoneBookUtil.h
@@ -1,4 +1,24 @@
 #ifndef SIADO2_PHONEBOOKUTIL_H
 #define SIADO2_PHONEBOOKUTIL_H
 
+#include "fileUtil.h"
+#include <iostream>
+
+using namespace std;
+
+struct PhoneUser{
+    string phone;
+    string address;
+    string name;
+};
+
+void convertTextToBinary(const string& textFilename, const string& binFilename);
+void convertBinaryToText(const string& binFilename, const string& textFilename);
+void printBinaryFile(const string& filename);
+PhoneUser getRecord(const string& filename, int n);
+void deleteRecord(const string& filename, int n);
+void putAllStartsWith(const string& binFilename, const string& textFilename, const &string start);
+void deleteAllStartsWith(const string& filename, const string& start);
+
+
 #endif //SIADO2_PHONEBOOKUTIL_H
